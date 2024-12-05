8366448e2c014cd8b4a85bffd38d6be1605eb292
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-06 00:08:44 +0500

    alpha

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                1: "#079294",
+            },
+        },
+    },
+    plugins: [],
+}
